a76b891f1939121a65bd518c168ac6e9b762789e
Sc0rch-thinks <ryanlin@skiff.com>
2024-08-02 12:23:06 +0800

    game: adding the car manger Scripts

---
+++ RunningLateGame/Assets/Scripts/ai car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class AICar : MonoBehaviour
+{
+    private CarController _carController;
+
+    private void Awake()
+    {
+        _carController = GetComponent<CarController>();
+    }
+
+    private void Update()
+    {
+        float forwardAmount = 1f;
+        float turnAmount = 1f;
+        _carController.SetInputs(forwardAmount,turnAmount);
+    }
+}
