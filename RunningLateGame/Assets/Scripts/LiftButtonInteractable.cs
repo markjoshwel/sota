2782374f448574d5caaddc2f4b85d70cd4b4001d
Mark Joshwel <mark@joshwel.co>
2024-08-10 20:11:35 +0800

    game(scripts): lift groundwork

---
+++ RunningLateGame/Assets/Scripts/LiftButtonInteractable.cs
@@ -0,0 +1,23 @@
+/*
+ * authors: mark joshwel
+ * date: 10/8/2024
+ * description: lift button behaviour implementation
+ */
+
+using UnityEngine;
+
+/// <summary>
+///     lift button behaviour implementation
+/// </summary>
+public class LiftButtonInteractable : CommonInteractable
+{
+    /// <summary>
+    ///     the lift the button is controlling
+    /// </summary>
+    [SerializeField] private LiftController liftController;
+
+    public override void Interact()
+    {
+        
+    }
+}
---
+++ RunningLateGame/Assets/Scripts/LiftController.cs
@@ -0,0 +1,14 @@
+/*
+ * authors: mark joshwel
+ * date: 10/8/2024
+ * description: lift behaviour implementation
+ */
+
+using UnityEngine;
+
+/// <summary>
+///     lift-ting behaviour for lifts
+/// </summary>
+public class LiftController : MonoBehaviour
+{
+}
