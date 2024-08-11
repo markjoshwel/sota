837699c68769d2107ac5518e0b27bd91bb2329aa
sippy-thinks <s10257006@connect.np.edu.sg>
2024-08-11 14:52:08 +0800

    game(ui,scripts): interim options menu

--- RunningLateGame/Assets/Scripts/ScreenOptionsMenu.cs
+++ RunningLateGame/Assets/Scripts/ScreenOptionsMenu.cs
@@ -17,6 +17,11 @@ public class ScreenOptionsMenu : CommonMenu
     public Button ButtonReturn;
 
     /// <summary>
+    ///     slider for master (music + sfx) volume
+    /// </summary>
+    public Slider SliderAudioMaster;
+
+    /// <summary>
     ///     slider for music volume
     /// </summary>
     public Slider SliderAudioMusic;
@@ -25,7 +30,7 @@ public class ScreenOptionsMenu : CommonMenu
     ///     slider for sfx volume
     /// </summary>
     public Slider SliderAudioSfx;
-
+    
     /// <summary>
     ///     function to associate a display state with the menu,
     ///     and subscribe button events to their respective functions
@@ -42,14 +47,21 @@ public class ScreenOptionsMenu : CommonMenu
         ButtonReturn.clicked += OptionReturnToMainMenu;
 
         // get the music slider from the ui root
-        SliderAudioMusic = UI.Q<Slider>("Music");
+        SliderAudioMaster = UI.Q<Slider>("MasterSlider");
+        // TODO: and set the initial value to the current music volume
+        // SliderAudioMusic.value = Audio.GetMusicVolume() * 100;
+        // and subscribe appropriate functions
+        SliderAudioMaster.RegisterCallback<ChangeEvent<float>>(OptionSetMasterVolume);
+        
+        // get the music slider from the ui root
+        SliderAudioMusic = UI.Q<Slider>("MusicSlider");
         // TODO: and set the initial value to the current music volume
         // SliderAudioMusic.value = Audio.GetMusicVolume() * 100;
         // and subscribe appropriate functions
         SliderAudioMusic.RegisterCallback<ChangeEvent<float>>(OptionSetMusicVolume);
-
+        
         // get the sfx slider from the ui root
-        SliderAudioSfx = UI.Q<Slider>("SFX");
+        SliderAudioSfx = UI.Q<Slider>("SFXSlider");
         // TODO: and set the initial value to the current sfx volume
         // SliderAudioSfx.value = Audio.GetSfxVolume() * 100;
         // and subscribe appropriate functions
@@ -65,12 +77,23 @@ public class ScreenOptionsMenu : CommonMenu
         // return to the main menu
         Game.SetDisplayState(GameManager.DisplayState.ScreenMainMenu);
     }
-
+    
     /// <summary>
     ///     handle music volume slider change,
     ///     sets the music channel volume in the audio manager appropriately
     /// </summary>
-    /// <param name="evt"></param>
+    /// <param name="evt">change event</param>
+    private void OptionSetMasterVolume(ChangeEvent<float> evt)
+    {
+        // TODO: slider is from 0 to 100, convert to 0 to 1, and set
+        // Audio.SetMusicVolume(evt.newValue / 100);
+    }
+    
+    /// <summary>
+    ///     handle music volume slider change,
+    ///     sets the music channel volume in the audio manager appropriately
+    /// </summary>
+    /// <param name="evt">change event</param>
     private void OptionSetMusicVolume(ChangeEvent<float> evt)
     {
         // TODO: slider is from 0 to 100, convert to 0 to 1, and set
@@ -81,7 +104,7 @@ public class ScreenOptionsMenu : CommonMenu
     ///     handle sfx volume slider change,
     ///     sets the sfx channel volume in the audio manager appropriately
     /// </summary>
-    /// <param name="evt"></param>
+    /// <param name="evt">change event</param>
     private void OptionSetSfxVolume(ChangeEvent<float> evt)
     {
         // TODO: slider is from 0 to 100, convert to 0 to 1, and set
